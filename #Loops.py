3dbdaf932c1db76ba3f61f4dcbc1c05bff912187
gitoutkai <kaihehau@hawaii.edu>
2025-05-04 14:08:11 -1000

    Update Loops.py
    This is the version I want to look at with rob

--- #Loops.py
+++ #Loops.py
@@ -4,7 +4,7 @@
 #may the fourth be with you 
 
 var1=0
-while var1<5:
+while var1<22:
     print(var1)
     print(var1)
     var1=var1+1
@@ -18,22 +18,45 @@ ultra_least_favorite_number=13
 while var1<20:
     if var1 == least_number:
         var1=var1+1
+        print(var1)
         continue
     if var1==ultra_least_favorite_number:
+        var1= var1 + 1
+        print(var1)
         continue
-    #  This break ends the code and stops the rest of the program from running for the (above) ultraleast_favorite_number    if (var1%2 == 1):
+    #  This continue sends the code reader back to before the indent
 
-var1=var1+1
+
 #Will print the variable var1 from var1=5 until it reaches 20               
-
-for var2 in range(5):
+print("Now we are printing variable 2 and 3. Variable 3 will print every integer 0,1,2,3,4. After Variable 3 prints, "
+      "Var2 will print every integer 0-49, besides 13. "
+      "Then the code will prompt a user input to change the range."
+      "If the range is expanded by the user, the code will run in the same way, but var2 will break at 420")
+for var2 in range(50):
+    if var2==ultra_least_favorite_number:
+        var2=var2+1
+        continue
     for var3 in range(5):
             print(var3)
     print(var2)
     print('')
-    if var2==ultra_least_favorite_number:
-        var2=var2+1
+
+var420=int(input("Enter a number between 1 and 500: "))
+
+
+for var2 in range(var420):
+    if var2 == 420:
         break
+    if var2 == ultra_least_favorite_number:
+        var2 = var2 + 1
+        continue
+    for var3 in range(5):
+        print(var3)
+    print(var2)
+    print('')
+     #break ends the code and stops the rest of the program from running for the (above) number
+print(''
+      '')
 
 print("done")
 
